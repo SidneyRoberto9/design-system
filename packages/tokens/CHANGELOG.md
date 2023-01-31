d819000f7b1f849d8b39d87a033da6cec444702b
SidneyRoberto9 <sidneyrpsilva@gmail.com>
2023-01-31 15:12:22 -0300

    🚧 chore:  Add new blue color in tokens

--- packages/tokens/CHANGELOG.md
+++ packages/tokens/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @sid-ui/tokens
 
+## 2.1.0
+
+### Minor Changes
+
+- Add New Blue Color
+
 ## 2.0.0
 
 ### Major Changes
--- packages/tokens/src/colors.ts
+++ packages/tokens/src/colors.ts
@@ -1,6 +1,7 @@
 export const colors = {
   white: '#FFFFFF',
   black: '#000000',
+  blue: '#00FFFF',
 
   gray100: '#E1E1E6',
   gray200: '#A9A9B2',
